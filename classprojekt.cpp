86683639e2f3817a7d80d86fa040c111451b59e7
Farschad Saberi <fasa@nb-saberi.ad.bbs-bassgeige.eu>
2020-08-19 07:02:22 +0200

    Rename function

--- classprojekt.cpp
+++ classprojekt.cpp
@@ -193,6 +193,23 @@ void ClassProjekt::setDuration(int duration)
     m_duration = duration;
 }
 
+QStringList ClassProjekt::identifierList()
+{
+    QStringList identList;
+    QMapIterator<int, ClassFrage> it(questionMap());
+    while (it.hasNext()) {
+        it.next();
+        ClassFrage question = it.value();
+        if(!question.identifier().isEmpty()){
+            QString ki = question.identifier();
+            if(!identList.contains(ki))
+                identList << ki;
+        }
+    }
+
+    return identList;
+}
+
 
 QDataStream &operator<<(QDataStream &out, const ClassProjekt &dat)
 {
